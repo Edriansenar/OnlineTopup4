13dea925944a9436850dc3e755fba61c63d9be16
Edriansenar <baconsmpofficial@gmail.com>
2025-04-11 10:21:58 +0800

    Update OnlineTopupDataService.cs

--- OnlineTopupDataService.cs
+++ OnlineTopupDataService.cs
@@ -9,12 +9,12 @@ namespace OnlineTopup
     public class OnlineTopupDataService
     {
 
-    List<OnlineTopupCommon> accounts = new List<OnlineTopupCommon>();
+        List<OnlineTopupCommon> accounts = new List<OnlineTopupCommon>();
 
-    public OnlineTopupDataService()
-    {
-        CreateDummyBankAccounts();
-    }
+        public OnlineTopupDataService()
+        {
+            CreateDummyBankAccounts();
+        }
         private void CreateDummyBankAccounts()
         {
             OnlineTopupCommon account1 = new OnlineTopupCommon();
@@ -22,13 +22,32 @@ namespace OnlineTopup
             account1.Pass = "12345";
 
             accounts.Add(account1);
-        }
 
-    public bool ValidateTopupAccount(string userAccount, string pass)
+            OnlineTopupCommon account2 = new OnlineTopupCommon
+            {
+                User = "indaleen",
+                Pass = "123",
+            };
+
+            accounts.Add(account2);
+
+            accounts.Add(new OnlineTopupCommon
+            {
+                User = "maria123",
+                Pass="admin",
+            });
+
+            accounts.Add(new OnlineTopupCommon
+            {
+                User = "ann123",
+                Pass = "admin123",
+            });
+        }
+        public bool ValidateTopupAccount(string userAccount, string userPass)
     {
         foreach (var account in accounts)
         {
-            if (account.User == userAccount && account.Pass == pass)
+            if (account.User == userAccount && account.Pass == userPass)
             {
                 return true;
             }
